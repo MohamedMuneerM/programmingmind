734995a014f540ed714248a7c1477356cbd4c775
MohamedMuneerM <rehanamujeebh@gmail.com>
2021-02-17 15:55:11 +0530

    added https forwarding and production settings

--- programmingmind/settings.py
+++ programmingmind/settings.py
@@ -40,49 +40,49 @@ EMAIL_SUBJECT_PREFIX = '[Programming Mind]'
 # Application definition
 
 INSTALLED_APPS = [
-    'mysite.apps.MysiteConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sitemaps',
-    'cloudinary_storage',
-    'cloudinary',
-    'crispy_forms',
-    'tinymce',
+	'mysite.apps.MysiteConfig',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'django.contrib.sitemaps',
+	'cloudinary_storage',
+	'cloudinary',
+	'crispy_forms',
+	'tinymce',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.cache.UpdateCacheMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.cache.FetchFromCacheMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.cache.UpdateCacheMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'programmingmind.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'programmingmind.wsgi.application'
@@ -92,10 +92,10 @@ WSGI_APPLICATION = 'programmingmind.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': BASE_DIR / 'db.sqlite3',
+	}
 }
 
 
@@ -103,47 +103,47 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
 
 TINYMCE_DEFAULT_CONFIG = {
-    'height': 360,
-    'width': 1000,
-    'cleanup_on_startup': True,
-    'custom_undo_redo_levels': 20,
-    'selector': 'textarea',
-    'theme': 'modern',
-    'plugins': '''
-            textcolor save link image media preview codesample contextmenu
-            table code lists fullscreen  insertdatetime  nonbreaking
-            contextmenu directionality searchreplace wordcount visualblocks
-            visualchars code fullscreen autolink lists  charmap print  hr
-            anchor pagebreak
-            ''',
-    'toolbar1': '''
-            fullscreen preview bold italic underline | fontselect,
-            fontsizeselect  | forecolor backcolor | alignleft alignright |
-            aligncenter alignjustify | indent outdent | bullist numlist table |
-            | link image media | codesample |
-            ''',
-    'toolbar2': '''
-            visualblocks visualchars |
-            charmap hr pagebreak nonbreaking anchor |  code |
-            ''',
-    'contextmenu': 'formats | link image',
-    'menubar': True,
-    'statusbar': True,
+	'height': 360,
+	'width': 1000,
+	'cleanup_on_startup': True,
+	'custom_undo_redo_levels': 20,
+	'selector': 'textarea',
+	'theme': 'modern',
+	'plugins': '''
+			textcolor save link image media preview codesample contextmenu
+			table code lists fullscreen  insertdatetime  nonbreaking
+			contextmenu directionality searchreplace wordcount visualblocks
+			visualchars code fullscreen autolink lists  charmap print  hr
+			anchor pagebreak
+			''',
+	'toolbar1': '''
+			fullscreen preview bold italic underline | fontselect,
+			fontsizeselect  | forecolor backcolor | alignleft alignright |
+			aligncenter alignjustify | indent outdent | bullist numlist table |
+			| link image media | codesample |
+			''',
+	'toolbar2': '''
+			visualblocks visualchars |
+			charmap hr pagebreak nonbreaking anchor |  code |
+			''',
+	'contextmenu': 'formats | link image',
+	'menubar': True,
+	'statusbar': True,
 }
 
 
@@ -161,9 +161,9 @@ LOGOUT_REDIRECT_URL = '/'
 # cloudinary
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
-    'API_KEY': os.environ.get('API_KEY'),
-    'API_SECRET': os.environ.get('API_SECRET'),
+	'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
+	'API_KEY': os.environ.get('API_KEY'),
+	'API_SECRET': os.environ.get('API_SECRET'),
 }
 
 
@@ -175,8 +175,8 @@ GOOGLE_RECAPTCHA_SECRET_KEY = '6Lffj_AZAAAAAJDjhlNVAk6zzXm4VmFWsU7PSbRq'
 
 CACHES = {
    'default': {
-      'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-      'LOCATION': '127.0.0.1:11211',
+	  'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+	  'LOCATION': '127.0.0.1:11211',
    }
 }
 
@@ -199,6 +199,13 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+#production settings
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 
 
